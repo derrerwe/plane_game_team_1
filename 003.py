d94a92ab0204908591dfc1c446ee10489388931a
derrerwe <2895809633@qq.com>
2025-06-25 11:17:01 +0800

    Update 003.py

--- 003.py
+++ 003.py
@@ -4,115 +4,130 @@ import pygame
 
 from pygame import *
 
-class HeroPlane(object):
+class HeroPlane(pygame.sprite.Sprite):
     def __init__(self,screen):
+        pygame.sprite.Sprite.__init__(self)
         #记录当前窗口对象
         self.screen = screen
+        #创建一个玩家飞机图片，当作真正的飞机
         self.player = pygame.image.load("D:\\code_python\\plane game\\飞机大战素材\\images\\me1.png")
-        self.x = 480 / 2 - 100 / 2
-        self.y = 500
+        #根据图片获取矩形对象
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [480/2-100/2,500]#x,y
+        #飞机速度
         self.speed = 10
         #子弹列表
-        self.bullets=[]
+        self.bullets=pygame.sprite.Group()
+
     # 监听键盘事件
     def key_control(self):
         key_pressed = pygame.key.get_pressed()
         if key_pressed[K_w] or key_pressed[pygame.K_UP]:
-            self.y -= self.speed
+            self.rect.top -= self.speed
         if key_pressed[K_a] or key_pressed[pygame.K_LEFT]:
-            self.x -= self.speed
+            self.rect.bottom -= self.speed
         if key_pressed[K_s] or key_pressed[pygame.K_DOWN]:
-            self.y += self.speed
+            self.rect.left += self.speed
         if key_pressed[K_d] or key_pressed[pygame.K_RIGHT]:
-            self.x += self.speed
+            self.rect.right += self.speed
         if key_pressed[K_SPACE]:
             #按下空格发射子弹
-            bullet=Bullet(self.screen,self.x,self.y)
+            bullet=Bullet(self.screen,self.rect.left,self.rect.top)
             #把子弹放到列表里
-            self.bullets.append(bullet)
+            self.bullets.add(bullet)
+    def update(self):
+        self.key_control()
+        self.display()
     def display(self):
         # 将飞机图片贴到窗口中
-        self.screen.blit(self.player,(self.x, self.y))
-        #遍历所有子弹
-        for bullet in self.bullets:
-            #让子弹飞，修改子弹的y坐标
-            bullet.auto_move()
-            #子弹显示在窗口
-            bullet.display()
+        self.screen.blit(self.player,self.rect)
+        #更新子弹坐标
+        self.bullets.update()
+        #把所有子弹全部添加到屏幕
+        self.bullets.draw(self.screen)
 
 
-class EnemyPlane(object):
+class EnemyPlane(pygame.sprite.Sprite):
     def __init__(self, screen):
         # 记录当前窗口对象
         self.screen = screen
         self.player = pygame.image.load("D:\\code_python\\plane game\\飞机大战素材\\images\\enemy1.png")  # 57*43
-        self.x = 0
-        self.y = 0
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [0,0]  # x,y
+        # 飞机速度
         self.speed = 10
         # 子弹列表
-        self.bullets = []
+        self.bullets = pygame.sprite.Group()
         # 敌军的移动方向
         self.direction = 'right'
 
     def display(self):
         # 将飞机图片贴到窗口中
-        self.screen.blit(self.player, (self.x, self.y))
-        #遍历所有子弹
-        for bullet in self.bullets:
-           #让子弹飞，修改子弹的y坐标
-           bullet.auto_move()
-           #子弹显示在窗口
-           bullet.display()
-
+        self.screen.blit(self.player, self.rect)
+        # 更新子弹坐标
+        self.bullets.update()
+        # 把所有子弹全部添加到屏幕
+        self.bullets.draw(self.screen)
+    def update(self):
+        self.auto_move()
+        self.auto_fire()
+        self.display()
     def auto_move(self):
         if self.direction == 'right':
-            self.x += self.speed
+            self.rect.right += self.speed
         elif self.direction == 'left':
-            self.x -= self.speed
-        if self.x > 480 - 57:
+            self.rect.right -= self.speed
+        if self.rect.right > 480 - 57:
             self.direction = 'left'
-        elif self.x < 0:
+        elif self.rect.right < 0:
             self.direction = 'right'
     def auto_fire(self):
         random_num=random.randint(1,10)
         if random_num ==1:
-            bullet=EnemyBullet(self.screen,self.x,self.y)
-            self.bullets.append(bullet)
+            bullet=EnemyBullet(self.screen,self.rect.left,self.rect.top)
+            self.bullets.add(bullet)
 
 #子弹类
-class Bullet(object):
+class Bullet(pygame.sprite.Sprite):
     def __init__(self,screen,x,y):
-        #坐标
-        self.x=x+100/2-10/2
-        self.y=y-11
-        #图片
+        #精灵类初始化
+        pygame.sprite.Sprite.__init__(self)
+        #创建图片
         self.image=pygame.image.load("D:\\code_python\\plane game\\飞机大战素材\\images\\bullet2.png")
+        #获取矩形对象
+        self.rect = self.image.get_rect()
+        self.rect.topleft=[x+100/2-10/2,y-11]
+
         #窗口
         self.screen = screen
+        #速度
         self.speed = 10
-    def display(self):
-        #显示子弹到窗口
-        self.screen.blit(self.image,(self.x, self.y))
-    def auto_move(self):
-        """让子弹飞"""
-        self.y -= self.speed
+    def update(self):
+        #修改子弹坐标
+        self.rect.top -= self.speed
+        #如果子弹移出屏幕上方，则销毁子弹对象
+        if self.rect.top<-11:
+            self.kill()
 
-class EnemyBullet(object):
+class EnemyBullet(pygame.sprite.Sprite):
     def __init__(self,screen,x,y):
-        #坐标
-        self.x=x+57/2-43/2
-        self.y=y-11
+        #初始化精灵类
+        pygame.sprite.Sprite.__init__(self)
         #图片
         self.image=pygame.image.load("D:\\code_python\\plane game\\飞机大战素材\\images\\bullet1.png")#5*11
+        #获取矩形对象
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x+57/2-43/2, y-11]
         #窗口
         self.screen = screen
         self.speed = 10
-    def display(self):
-        #显示子弹到窗口
-        self.screen.blit(self.image,(self.x, self.y))
-    def auto_move(self):
-        """让子弹飞"""
-        self.y += self.speed
+
+    def update(self):
+        # 修改子弹坐标
+        self.rect.top += self.speed
+        # 如果子弹移出屏幕上方，则销毁子弹对象
+        if self.rect.top > 852:
+            self.kill()
 
 class GameSound(object):
     def __init__(self):
@@ -121,7 +136,7 @@ class GameSound(object):
         pygame.mixer_music.set_volume(0.5)#声音大小
     def playBackgroundMusic(self):
         pygame.mixer.music.play(-1)#循环播放音乐
-class
+
 def main():
     """完成整个程序的控制"""
     sound=GameSound()
